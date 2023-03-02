7b5afeaa0bdfaa2eb0e70263e4fd5a888fea96ff
Paul Nykiel <paul.nykiel@gmail.com>
2023-03-02 19:47:28 +0100

    Added interrupt declarations

--- System/avr/interrupt.h
+++ System/avr/interrupt.h
@@ -12,4 +12,17 @@ void sei(void);
 
 #define ISR(name) void name(void)
 
+ISR(TIMER0_OVF_vect);
+ISR(TIMER2_OVF_vect);
+ISR(TWI_vect);
+ISR(SPI_STC_vect);
+ISR(USART0_RX_vect);
+ISR(USART0_TX_vect);
+ISR(USART1_RX_vect);
+ISR(USART1_TX_vect);
+ISR(USART2_RX_vect);
+ISR(USART2_TX_vect);
+ISR(USART3_RX_vect);
+ISR(USART3_TX_vect);
+
 #endif // MOCK_AVR_INTERRUPT_H
